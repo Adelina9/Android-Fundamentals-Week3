12c7ec5a4c0dafc026b3a713ee78da480a22be2e
Adelina <adelina.girlea@gmail.com>
2020-05-02 21:15:07 +0300

    Added toast for spinner

--- app/src/main/java/com/example/assignment2/SpinnerActivity.java
+++ app/src/main/java/com/example/assignment2/SpinnerActivity.java
@@ -3,13 +3,16 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SpinnerActivity extends AppCompatActivity {
+public class SpinnerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner spinner;
     private List<String> sweets;
 
@@ -20,7 +23,15 @@ public class SpinnerActivity extends AppCompatActivity {
         spinner = findViewById(R.id.spinnerSweets);
         setSpinnerSource();
         spinner.setAdapter(getSpinnerAdapter());
+        spinner.setOnItemSelectedListener(this);
     }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) { }
 
     private void setSpinnerSource() {
         sweets = new ArrayList<>();
